e882efdc0d537cb0d66fe494a3e926abbd5ef480
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 23:28:01 +0100

    update the jwt token content

--- src/auth/services/signIn.user.ts
+++ src/auth/services/signIn.user.ts
@@ -1,11 +1,14 @@
 import * as moment from "moment";
 
 import { ControllerArgs, HttpStatus, UnAuthorizedError, compareHashedData, computeExpiryDate, config, generateToken } from "../../core";
-import { User } from "../../users";
+import { User, UserRoles } from "../../users";
 
 
 export class SignIn {
-    constructor(private readonly db: typeof User) {}
+    constructor(
+        private readonly db: typeof User,
+        private readonly dbUserRoles: typeof UserRoles
+    ) {}
 
 
     sign = async ({input}: ControllerArgs) => {
@@ -16,20 +19,26 @@ export class SignIn {
             .findOne({ where: { email } }))?.toJSON();
         if (!user) throw new UnAuthorizedError('Invalid login credentials');
 
+        const userRole = await this.dbUserRoles.findOne({
+            where: {
+                role_id: user.role,
+            }
+        })
+
         const isValid = await compareHashedData(password, user.password!);
         if (!isValid) throw new UnAuthorizedError('Invalid login credentials');
 
         const now = Date.now();
         const accessTokenExpiresIn = moment(now).add(parseInt(config.auth.accessTokenExpiresIn), 'minutes');
         const accessToken = generateToken(
-            { id: user.id, role: user.role },
+            { id: user.id, role: userRole.name },
             config.auth.accessTokenSecret,
             accessTokenExpiresIn.unix().toString()
         );
         
         const refreshTokenExpiresIn = moment(now).add(parseInt(config.auth.refreshTokenExpiresIn), 'hours')
         const refreshToken = generateToken(
-            { id: user.id, role: user.role },
+            { id: user.id, role: userRole.name },
             config.auth.refreshTokenSecret,
             refreshTokenExpiresIn.unix().toString()
         );
