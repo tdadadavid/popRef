c888922fc524031c8834fc45d1208437efe92a0e
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 23:59:17 +0100

    final commit

--- src/projects/router/project.router.ts
+++ src/projects/router/project.router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { controllerHandler } from "../../core/middlewares";
-import { seeProjects } from "../services";
+import { seeProjectTxn, seeProjects } from "../services";
 
 export const projectRouter = Router();
 
 projectRouter
     .get('/', controllerHandler.handle(seeProjects.see))
-    .get('/:project_id/transactions', controllerHandler.handle(()=>{}))
-    .get('/:project_id/contributions', controllerHandler.handle(()=>{}))
+    .get('/:token_id/transactions', controllerHandler.handle(seeProjectTxn.see))
--- src/projects/services/index.ts
+++ src/projects/services/index.ts
@@ -1,4 +1,6 @@
-import { ProjectApprovalStatus, Projects } from "../model";
+import { ProjectApprovalStatus, ProjectContributions, ProjectTransactions, Projects } from "../model";
+import { SeeTransactions } from "./see.project.transactions";
 import { SeeProjects } from "./see.projects";
 
 export const seeProjects = new SeeProjects(Projects, ProjectApprovalStatus);
+export const seeProjectTxn = new SeeTransactions(ProjectContributions, ProjectTransactions)
