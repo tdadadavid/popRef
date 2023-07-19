1ace6cd2257061612edb6a03e8ea88a6a8674951
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 22:38:12 +0100

    create routes for admins

--- src/users/admins/router/admin.routes.ts
+++ src/users/admins/router/admin.routes.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { currentUser } from "src/auth";
+import { controllerHandler } from "src/core";
+import { isAdmin } from "src/users/middlewares";
+import { createRole, decideOnProposal, seeProjectProposals } from "../services";
 
 const adminRouter = Router();
+
+
+adminRouter
+    .use([currentUser.handle, isAdmin])
+    .post('/decision', controllerHandler.handle(decideOnProposal.make))
+    .post('/roles', controllerHandler.handle(createRole.create))
+    .get('/proposals', controllerHandler.handle(seeProjectProposals.view));
