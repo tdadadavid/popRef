7a290f0df62eb4e81cea7f9a05170b222681088e
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 10:32:19 +0100

    add health endpoint

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,7 +1,16 @@
-import { Router } from "express";
+import { Request, Router, Response } from "express";
 import { authRouter } from "../auth";
+import { HttpStatus } from "../core";
 
 export const appRouter = Router();
 
+
 appRouter.use("/auth", authRouter);
 
+
+appRouter.get('/health', (_: Request, res: Response) => {
+    res.status(HttpStatus.OK).json({
+        message: "App up",
+        version: "1.0",
+    })
+});
