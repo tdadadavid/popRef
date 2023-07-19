888c4456d8605b1eeb4266a08f5f705b52d4fc8a
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 10:31:47 +0100

    add test for health endpoint

---
+++ test/app.spec.ts
@@ -0,0 +1,15 @@
+import { createServer } from "http"; 
+import request from "supertest";
+
+import { app } from "../src/app"
+
+describe('App Health', () => {
+
+    const server = createServer(app);
+
+
+    it('should return 200 Ok response when the /health endpoint is down', async () => {
+        const response = request(server).get('/api/v1/health');
+        expect(response.statusCode).toBe(200);
+    })
+})
