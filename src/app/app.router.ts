e541800c0d38aba64a48646c742a014efe7b46b2
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 22:07:34 +0100

    add new routes to app router

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,11 +1,16 @@
 import { Request, Router, Response } from "express";
+
 import { authRouter } from "../auth";
 import { HttpStatus } from "../core";
+import { userRouter } from "../users";
+import { projectRouter } from "../projects";
 
 export const appRouter = Router();
 
 
 appRouter.use("/auth", authRouter);
+appRouter.use("/users", userRouter);
+appRouter.use("/tokens", projectRouter);
 
 
 appRouter.get('/health', (_: Request, res: Response) => {
