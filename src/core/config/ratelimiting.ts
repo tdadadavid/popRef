c67df439b088b55548afbb55d990e3fd9535226d
tdadadavid <davidtofunmidada@gmail.com>
2023-07-19 23:27:30 +0100

    add rate limit for project proposals

--- src/core/config/ratelimiting.ts
+++ src/core/config/ratelimiting.ts
@@ -17,3 +17,11 @@ export const authRateLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
 })
+
+export const submitPrposalLimiter = rateLimit({
+    windowMs: moment().add(6, 'hours').unix(), // 24 hrs in milliseconds
+    max: 20, // you can submit only 2 proposals in a day
+    message: 'You have exceeded the 10 requests in 24 hrs limit!', 
+    standardHeaders: true,
+    legacyHeaders: false,
+})
